96ba22a50bb493e09d0bd4b17e7b18ff2dc03387
swetha3456 <swetha.murali@iiitb.ac.in>
2024-04-09 12:34:58 +0530

    register page backend

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask, render_template, session, url_for, redirect, request, flash
 from dbfunctions import *
-import sqlite3
-
-conn = sqlite3.connect("database.db")
-cur = conn.cursor()
 
 app = Flask(__name__)
 app.secret_key = b"030a8ee0eb274b3e7fd9db490b0fd6a532b1fa1f1fd6825c5852c7363358c4b6"
@@ -36,19 +32,31 @@ def exercise():
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     if request.method == "POST":
-        if authenticate_user(request.form["username"], request.form["password"], cur):
+        if authenticate_user(request.form["username"], request.form["logpass"]):
             session["username"] = request.form["username"]
-            print(request.form["username"])
+            return redirect(url_for("home"))
         else:
             flash("Invalid credentials")
-
-        return redirect(url_for("home"))
 
     return render_template("login.html")
 
 @app.route("/register", methods = ["GET", "POST"])
 def register():
     if request.method == "POST":
+        context = {
+            "username" : request.form["username"],
+            "password" : request.form["logpass"],
+            "age" : int(request.form["Age"]),
+            "weight" : float(request.form["weight"]),
+            "goals" : request.form["goals"],
+            "period_duration" : request.form["duration"],
+            "conditions" : request.form["conditions"],
+            "date" : request.form["period-date"],
+            "cycle_length" : request.form["cycle-length"],
+            "difficulty" : request.form["difficulty"]
+        }
+
+        register_user(context)
         return redirect(url_for("home"))
 
     return render_template("signin.html")
--- dbfunctions.py
+++ dbfunctions.py
@@ -16,3 +16,27 @@ def authenticate_user(username, password):
     expected_password = list(cursor.execute(f"select password from users where username='{username}';"))[0][0]
     conn.close()
     return password == expected_password
+
+def register_user(context):
+    command = f"""insert into users values(
+        '{context["username"]}',
+        '{context["password"]}',
+        {context["age"]},
+        {context["weight"]},
+        {context["cycle_length"]},
+        {context["period_duration"]},
+        '{context["date"]}',
+        '{context["difficulty"]}',
+        '{context["goals"]}',
+        '{context["conditions"]}'
+    );"""
+
+    print(command)
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute(command)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    init_db()
